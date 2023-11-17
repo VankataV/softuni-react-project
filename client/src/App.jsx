a3917958e8550a240682e817e8e9734a8305b53b
VankataV <127044696+VankataV@users.noreply.github.com>
2023-11-17 15:26:05 +0200

    create Theaters component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom';
 
 import Header from "./components/header/Header"
 import Home from './components/home/Home';
+import Theaters from './components/theaters/Theaters';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
 
             <Routes>
                     <Route path='/' element={<Home />} />
+                    <Route path='/theaters' element={<Theaters />} />
             </Routes>
         </>
     )
