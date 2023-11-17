fa7941297b084ea11e8310238be043a06908c2b2
VankataV <127044696+VankataV@users.noreply.github.com>
2023-11-17 18:27:30 +0200

    Add Add form and css

--- client/src/components/add/Add.jsx
+++ client/src/components/add/Add.jsx
@@ -1,25 +1,25 @@
+import styles from './Add.module.css'
+
 export default function Add() {
     return (
         <>
-            <section id="create-page" className="auth">
-                <form id="create" >
-                    <div className="container">
-                        <h1>Add a play </h1>
-                        <label htmlFor="play-title">Play title:</label>
-                        <input type="text" id="title" name="title" placeholder="Enter play title..." />
+            <form id="add" className={styles.form}>
+                <div className="container">
+                    <h1>Add a play </h1>
+                    <label htmlFor="play-title">Play title</label>
+                    <input type="text" id="title" name="title" placeholder="Enter play title..." />
 
-                        <label htmlFor="producer">Producer:</label>
-                        <input type="text" id="producer" name="producer" placeholder="Enter producer name..." />
+                    <label htmlFor="producer">Producer</label>
+                    <input type="text" id="producer" name="producer" placeholder="Enter producer name..." />
 
-                        <label htmlFor="play-img">Image:</label>
-                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+                    <label htmlFor="play-img">Image</label>
+                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
 
-                        <label htmlFor="description">Description:</label>
-                        <textarea name="description" id="description"></textarea>
-                        <input className="btn submit" type="submit" value="Add play" />
-                    </div>
-                </form>
-            </section>
+                    <label htmlFor="description">Description</label>
+                    <textarea name="description" id="description"></textarea>
+                    <input className="btn submit" type="submit" value="Add play" />
+                </div>
+            </form>
         </>
     )
 }
