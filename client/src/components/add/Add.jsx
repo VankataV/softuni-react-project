03cedc4d0bffae9d4d4369b59802610e82d28806
VankataV <127044696+VankataV@users.noreply.github.com>
2023-11-17 19:00:50 +0200

    Add Add functionality

--- client/src/components/add/Add.jsx
+++ client/src/components/add/Add.jsx
@@ -1,18 +1,27 @@
 import styles from './Add.module.css'
+import {get, post} from '../../lib/requests.js'
+
+const addSubmitHandler=async (e)=>{
+    e.preventDefault()
+
+    const data=Object.fromEntries(new FormData(e.currentTarget))
+    await post(data)
+
+}
 
 export default function Add() {
     return (
         <>
-            <form id="add" className={styles.form}>
+            <form id="add" className={styles.form} onSubmit={addSubmitHandler}>
                 <div className="container">
                     <h1>Add a play </h1>
-                    <label htmlFor="play-title">Play title</label>
+                    <label htmlFor="title">Play title</label>
                     <input type="text" id="title" name="title" placeholder="Enter play title..." />
 
                     <label htmlFor="producer">Producer</label>
                     <input type="text" id="producer" name="producer" placeholder="Enter producer name..." />
 
-                    <label htmlFor="play-img">Image</label>
+                    <label htmlFor="imageUrl">Image</label>
                     <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
 
                     <label htmlFor="description">Description</label>
