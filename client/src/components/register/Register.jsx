6f9d4f3e25596f0f4a148e39fa62ce307d69319d
VankataV <127044696+VankataV@users.noreply.github.com>
2023-11-23 11:47:02 +0200

    Add Register form and css

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,7 +1,32 @@
-export default function Register(){
-    return(
+import styles from "./Register.module.css"
+import { Link } from 'react-router-dom'
+
+
+export default function Register() {
+    return (
         <>
-        
+            <form id="add" className={styles.form}>
+                <div className="container">
+                    <h1>Register </h1>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName" name="firstName" placeholder="First Name" />
+
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName" name="lastName" placeholder="Last Name" />
+
+                    <label htmlFor="email">Email address</label>
+                    <input type="text" id="email" name="email" placeholder="Your email" />
+
+                    <label htmlFor="password">Password</label>
+                    <input type="text" id="password" name="password" placeholder="Your password" />
+                    
+                    <label htmlFor="rePassword">Repeat Password</label>
+                    <input type="text" id="rePassword" name="rePassword" placeholder="Repeat password" />
+
+                    <input className={styles.button} type="submit" value="Register" />
+                    <Link className={styles.link} to='/login'>Already have an account?</Link>
+                </div>
+            </form>
         </>
     )
 }
