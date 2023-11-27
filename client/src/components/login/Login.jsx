4833903fd4371c50dcbe4702ebc02e5a39a9e0c3
VankataV <127044696+VankataV@users.noreply.github.com>
2023-11-27 11:25:01 +0200

    Add Login component and css

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,7 +1,20 @@
-export default function Login(){
-    return(
+import styles from "./Login.module.css"
+import { Link } from 'react-router-dom'
+
+export default function Login() {
+    return (
         <>
-        
+            <form id="add" className={styles.form}>
+                    <h1>Login </h1>
+                    <label htmlFor="email">Email address</label>
+                    <input type="text" id="email" name="email" placeholder="Your email" />
+
+                    <label htmlFor="password">Password</label>
+                    <input type="text" id="password" name="password" placeholder="Your password" />
+
+                    <input className={styles.button} type="submit" value="Login" />
+                    <Link className={styles.link} to='/login'>Don't have an account?</Link>
+            </form>
         </>
     )
 }
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -6,26 +6,24 @@ export default function Register() {
     return (
         <>
             <form id="add" className={styles.form}>
-                <div className="container">
-                    <h1>Register </h1>
-                    <label htmlFor="firstName">First Name</label>
-                    <input type="text" id="firstName" name="firstName" placeholder="First Name" />
+                <h1>Register </h1>
+                <label htmlFor="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" placeholder="First Name" />
 
-                    <label htmlFor="lastName">Last Name</label>
-                    <input type="text" id="lastName" name="lastName" placeholder="Last Name" />
+                <label htmlFor="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" placeholder="Last Name" />
 
-                    <label htmlFor="email">Email address</label>
-                    <input type="text" id="email" name="email" placeholder="Your email" />
+                <label htmlFor="email">Email address</label>
+                <input type="text" id="email" name="email" placeholder="Your email" />
 
-                    <label htmlFor="password">Password</label>
-                    <input type="text" id="password" name="password" placeholder="Your password" />
-                    
-                    <label htmlFor="rePassword">Repeat Password</label>
-                    <input type="text" id="rePassword" name="rePassword" placeholder="Repeat password" />
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password" name="password" placeholder="Your password" />
 
-                    <input className={styles.button} type="submit" value="Register" />
-                    <Link className={styles.link} to='/login'>Already have an account?</Link>
-                </div>
+                <label htmlFor="rePassword">Repeat Password</label>
+                <input type="text" id="rePassword" name="rePassword" placeholder="Repeat password" />
+
+                <input className={styles.button} type="submit" value="Register" />
+                <Link className={styles.link} to='/login'>Already have an account?</Link>
             </form>
         </>
     )
