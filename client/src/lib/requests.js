2d94b2c8a3d14d3d039e7caee7da48de37efb8b3
VankataV <127044696+VankataV@users.noreply.github.com>
2023-12-04 21:03:01 +0200

    Migrate to collections

--- client/src/lib/requests.js
+++ client/src/lib/requests.js
@@ -1,46 +1,54 @@
-const baseUrl='http://localhost:3030/jsonstore'
+const baseUrl = 'http://localhost:3030/data'
 
-async function get(){
-    const response=await fetch(`${baseUrl}/theaters`)
+const token = localStorage.getItem('accessToken')
 
-    if(response.status===204){
+async function get() {
+    const response = await fetch(`${baseUrl}/theaters`)
+
+    if (response.status === 204) {
         return {}
     }
 
-    const result=await response.json()
+    const result = await response.json()
 
     return result
 }
 
-async function post(data){
-    await fetch(`${baseUrl}/theaters`,{
-        method:'POST',
-        'content-type':'application/json',
-        body:JSON.stringify(data)
+async function post(data) {
+    await fetch(`${baseUrl}/theaters`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify(data)
     })
 }
 
-async function put(data){
-    await fetch(`${baseUrl}/theaters`,{
-        method:"PUT",
-        'content-type':'application/json',
-        body:JSON.stringify(data)
+async function put(data) {
+    await fetch(`${baseUrl}/theaters`, {
+        method: "PUT",
+        headers: {
+            'content-type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify(data)
     })
 }
 
-async function del(){
-    await fetch(`${baseUrl}/theaters`,{
-        method:"DELETE"
+async function del() {
+    await fetch(`${baseUrl}/theaters`, {
+        method: "DELETE"
     })
 }
 
-async function getAll(){
-    const result =await get()
+async function getAll() {
+    const result = await get()
 
     return Object.values(result)
 }
 
-export{
+export {
     get,
     post,
     put,
