a1db9ff485e435e3496b2e812d842284e88a5faa
VankataV <127044696+VankataV@users.noreply.github.com>
2023-12-04 22:01:26 +0200

    Add PlayDetails structure and css

--- client/src/components/playDetails/PlayDetails.jsx
+++ client/src/components/playDetails/PlayDetails.jsx
@@ -1,3 +1,15 @@
-export default function PlayDetails(){
-    
+import * as styles from './PlayDetails.module.css'
+import image from '../../images/logo.png'
+export default function PlayDetails() {
+    return (
+        <>
+            <div className={styles.div}>
+                {/* <div className={styles.imgDiv}><img src={image}/></div> */}
+                <div className={styles.titleDiv}>title</div>
+                <div className={styles.producerDiv}>producer</div>
+                <div className={styles.theatreDiv}>theatre</div>
+                <div className={styles.descriptionDiv}>description</div>
+            </div>
+        </>
+    )
 }
