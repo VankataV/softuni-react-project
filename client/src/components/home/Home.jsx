19a6e49163ed2c35e2b07c58cb53dda8128c279b
VankataV <127044696+VankataV@users.noreply.github.com>
2023-12-06 16:55:58 +0200

    Add Home component and css

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,7 +1,15 @@
-export default function Home(){
+import './Home.css'
+
+export default function Home() {
     return (
         <>
-        
+            <div className="banner">
+                <div className="banner-background"></div>
+                <div className="banner-text-container">
+                    <h1 className="banner-title">Welcome to The Theatre!</h1>
+                    <p className="banner-text">Experience the Magic of Live Theatre! Discover captivating plays available now at The Theatre. Step into a world of drama, comedy, and artistry - explore our curated selection of current theatrical performances and indulge in the thrill of live entertainment.</p>
+                </div>
+            </div>
         </>
     )
 }
